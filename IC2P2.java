bf5773d51c38594763ca6a4869e30f909fc8b773
ocairob <50335618+ocairob@users.noreply.github.com>
2020-07-09 08:59:05 -0500

    Add files via upload

---
+++ IC2P2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IC2P2 {
+     public static void main(String[] args) {
+        int whole1, whole2, whole3, whole4; 
+        Scanner lee = new Scanner(System.in);
+        System.out.println("Please enter four whole numbers: ");
+        whole1 = lee.nextInt();
+        whole2 = lee.nextInt();
+        whole3 = lee.nextInt();
+        whole4 = lee.nextInt();
+        System.out.println(whole4 +"\t"+ whole3 +"\t"+ whole2 +"\t"+ whole1);
+       }
+    
+}
